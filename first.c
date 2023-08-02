93855e20a0dac9534d9d05a36ea8d59bc55fe3d0
manandetroja <141203183+manandetroja@users.noreply.github.com>
2023-08-02 09:23:40 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("Name    =\t\"manan\" \n");
+	printf("\fSem     =\t\a\"First\"\n");
+	printf("\fRoll no =\t\"150\"\n");
+	printf("\fAddress =\t\"201 Avenue, ambika township\v\t\t nana mova road rajkot\"\n");
+	return 0;
+} 
